a01f1b60ec4e159040c341295113b9481463eef3
Westefns Damião de Souza Silva <westefns@outlook.com.br>
2020-05-08 23:14:19 -0300

    Comaçando o Handler

---
+++ Todo.Domain/Handlers/TodoHandler.cs
@@ -0,0 +1,25 @@
+using Flunt.Notifications;
+using Todo.Domain.Commands;
+using Todo.Domain.Commands.Contracts;
+using Todo.Domain.Handlers.Contracts;
+using Todo.Domain.Repositories;
+
+namespace Todo.Domain.Handlers
+{
+    public class TodoHandler : 
+        Notifiable,
+        IHandler<CreateTodoCommand>
+    {
+        private readonly ITodoRepository _repository;
+
+        public TodoHandler(ITodoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ICommandResult Handle(CreateTodoCommand command)
+        {
+            
+        }
+    }
+}
